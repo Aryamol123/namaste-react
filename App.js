92bbd70bde3fb3224f3820077c98b7b9533bf5d2
aryaleadmetrics <aryamol@leadmetrics.ai>
2026-04-11 19:25:46 +0530

    EP1

---
+++ App.js
@@ -0,0 +1,16 @@
+{/* <div id="heading">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
+    </div>
+</div> */}
+
+
+
+// const heading = React.createElement("h1",{id:"heading"},"Hello World from React");
+const heading = React.createElement("div",{id:"heading"},React.createElement("div",{id:"child"},[React.createElement("h1",null,"I am h1 tag"),React.createElement("h2",null,"I am h2 tag")]));
+
+
+     console.log('heading', heading)
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
